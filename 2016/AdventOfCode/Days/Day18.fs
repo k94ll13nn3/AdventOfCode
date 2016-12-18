4a8cb6e9b7903ec839c95fb7d65d5c6e3fca2a06
Keuvain <sheepinadev@outlook.fr>
2016-12-18 13:17:32 +0100

    Move to full bool mode for Day 18

--- 2016/AdventOfCode/Days/Day18.fs
+++ 2016/AdventOfCode/Days/Day18.fs
@@ -1,20 +1,21 @@
 module Day18
 
 let input = "^..^^.^^^..^^.^...^^^^^....^.^..^^^.^.^.^^...^.^.^.^.^^.....^.^^.^.^.^.^.^.^^..^^^^^...^.....^....^."
+let input2 = input.ToCharArray() |> Array.map (fun elem -> elem = '.')
 
-let getNextVal (map : char[]) (i : int) =
+let getNextVal (map : bool[]) (i : int) =
     match i with
-    | _ when i = 0 -> if '.' = map.[i + 1] then '.' else '^'
-    | _ when i + 1 = input.Length -> if map.[i - 1] = '.' then '.' else '^'
-    | _ -> if map.[i - 1] = map.[i + 1] then '.' else '^'
+    | _ when i = 0 -> map.[i + 1]
+    | _ when i + 1 = input.Length -> map.[i - 1]
+    | _ -> map.[i - 1] = map.[i + 1]
 
-let getNextLine (previousLine : char[]) =
-    previousLine |> Array.mapi (fun i _ -> getNextVal previousLine i)
+let getNextLine (previousLine : bool[]) = previousLine |> Array.mapi (fun i _ -> getNextVal previousLine i)
+
+let countTrue a = a |> Array.filter id |> Array.length
 
 let compute (rows : int) =
-    seq {1..rows} // this will compute one more line in order to have the result as the second element of the accumulator
-    |> Seq.fold (fun acc _ ->  let (a, b) = acc in ((getNextLine a), b + (a |> Array.filter (fun c -> c = '.') |> Array.length))) (input.ToCharArray(), 0)
-    |> snd
+    seq {1..rows-1} // this will compute one more line in order to have the result as the second element of the accumulator
+    |> Seq.fold (fun acc _ ->  let (a, b) = acc in let c = getNextLine a in (c, b + (c |> countTrue))) (input2, (input2 |> countTrue)) |> snd
 
 let computeFirst = compute 40
 let computeSecond = compute 400000
