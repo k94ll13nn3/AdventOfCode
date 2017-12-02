a3216fb1a8e0464bc42d0bf3e45e3a30e75c0800
Keuvain <k94ll13nn3@outlook.fr>
2017-12-02 09:39:46 +0100

    2016 - Day 1

---
+++ 2017/Days/Day1.fs
@@ -0,0 +1,16 @@
+module Day1
+
+open Helper
+
+let compute (ls:char[]) (offset:int) =
+    ls
+    |> Array.mapi (fun i e -> if e = ls.[(i + offset) % ls.Length] then (charToInt e) else 0)
+    |> Array.sum
+
+let computeFirst =
+    let ls = readInput "input1.txt"
+    compute (ls |> Array.ofSeq) 1
+
+let computeSecond =
+    let ls = readInput "input1.txt"
+    compute (ls |> Array.ofSeq) (ls.Length/2)
