34a5bdf6047940bac9f22f2620613caa13001cbf
Keuvain <k94ll13nn3@outlook.fr>
2017-12-02 09:39:46 +0100

    2016 - Day 1

---
+++ 2017/Core/Helper.fs
@@ -0,0 +1,24 @@
+module Helper
+
+open System.IO
+open System.Text.RegularExpressions
+
+let (+/) path1 path2 = Path.Combine(path1, path2)
+
+let readLines filePath = 
+    filePath
+    |> (+/) @"Inputs\"
+    |> File.ReadAllLines
+
+let readInput filePath = 
+    filePath
+    |> (+/) @"Inputs\"
+    |> File.ReadAllText
+
+//http://fssnip.net/29
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
+
+let inline charToInt (c:char) = int c - int '0'
---
+++ 2017/Program.fs
@@ -0,0 +1,9 @@
+﻿module AdventOfCode
+
+open Day1
+
+[<EntryPoint>]
+let main argv =
+    computeFirst |> printfn "%A"
+    computeSecond |> printfn "%A"
+    0 // return an integer exit code
