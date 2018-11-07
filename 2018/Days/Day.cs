63da39fddb6b9055dad7832442f9ff7a4cf3c9f9
Keuvain <k94ll13nn3@outlook.fr>
2018-11-08 00:30:47 +0100

    Add skeleton for 2018

---
+++ 2018/Days/Day.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode.Days
+{
+    public abstract class Day
+    {
+        public static readonly Day Empty = new DummyDay();
+
+        private readonly string _dayNumber;
+
+        private string _content;
+
+        private string[] _lines;
+
+        protected Day() => _dayNumber = GetType().Name.Substring(3);
+
+        public ReadOnlySpan<char> GetContent()
+        {
+            if (_content is null)
+            {
+                _content = File.ReadAllText($"Inputs/Input{_dayNumber}.txt");
+            }
+
+            return _content.AsSpan();
+        }
+
+        public IEnumerable<ReadOnlyMemory<char>> GetLines()
+        {
+            if (_lines is null)
+            {
+                _lines = File.ReadAllLines($"Inputs/Input{_dayNumber}.txt");
+            }
+
+            return GetLinesImpl();
+
+            IEnumerable<ReadOnlyMemory<char>> GetLinesImpl()
+            {
+                foreach (string line in _lines)
+                {
+                    yield return line.AsMemory();
+                }
+            }
+        }
+
+        public abstract string ProcessFirst();
+
+        public abstract string ProcessSecond();
+
+        private class DummyDay : Day
+        {
+            public override string ProcessFirst() => "Dummy";
+
+            public override string ProcessSecond() => "Dummy";
+        }
+    }
+}
---
+++ 2018/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using AdventOfCode.Days;
+
+namespace AdventOfCode
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var type = Type.GetType($"AdventOfCode.Days.Day{args[0]}");
+            Day day = type is null ? Day.Empty : (Day)Activator.CreateInstance(type);
+
+            var timer = new Stopwatch();
+
+            timer.Start();
+            string firstPart = day.ProcessFirst();
+            timer.Stop();
+            Console.WriteLine($"First problem: {firstPart} in {timer.Elapsed.Seconds}s");
+
+            timer.Restart();
+            string secondPart = day.ProcessSecond();
+            timer.Stop();
+            Console.WriteLine($"Second problem: {secondPart} in {timer.Elapsed.Seconds}s");
+        }
+    }
+}
