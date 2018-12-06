455a882455075771bec5527874e4cfaff9e0f20d
Keuvain <k94ll13nn3@outlook.fr>
2018-12-06 19:53:02 +0100

    Optimize days 5 and 6

--- 2018/Days/Day6.cs
+++ 2018/Days/Day6.cs
@@ -16,24 +16,40 @@ namespace AdventOfCode.Days
                 .Select((x, i) => (ind: i, x: int.Parse(x[0]), y: int.Parse(x[1])))
                 .ToList();
 
-            var (xMin, xMax, yMin, yMax) = (points.Select(x => x.x).Min(),points.Select(x => x.x).Max(), points.Select(x => x.y).Min(), points.Select(x => x.y).Max());
+            var (xMin, xMax, yMin, yMax) = (points.Select(x => x.x).Min(), points.Select(x => x.x).Max(), points.Select(x => x.y).Min(), points.Select(x => x.y).Max());
             var dico = new Dictionary<int, int>();
 
             for (int i = xMin; i <= xMax; i++)
             {
                 for (int j = yMin; j <= yMax; j++)
                 {
-                    var distances = points.Select((x, ind) => (d: Distance((x.x, x.y), (i, j)), ind: ind)).OrderBy(x => x.d);
-                    if (distances.First().d != distances.Skip(1).First().d)
+                    int index = -1;
+                    int minimum = int.MaxValue;
+                    int minimumSeen = int.MaxValue;
+                    for (int k = 0; k < points.Count; k++)
                     {
-                        var t = distances.First().ind;
-                        if (dico.ContainsKey(t))
+                        var d = Distance(points[k].x, points[k].y, i, j);
+                        if (d == minimum)
                         {
-                            dico[t]++;
+                            index = -1;
+                            minimumSeen = minimum;
+                        }
+                        else if (d < minimum)
+                        {
+                            minimum = d;
+                            index = k;
+                        }
+                    }
+
+                    if (index != -1 && minimum != minimumSeen)
+                    {
+                        if (dico.ContainsKey(index))
+                        {
+                            dico[index]++;
                         }
                         else
                         {
-                            dico[t] = 1;
+                            dico[index] = 1;
                         }
                     }
                 }
@@ -49,19 +65,28 @@ namespace AdventOfCode.Days
 
         public override string ProcessSecond()
         {
-             var points = GetLinesAsStrings()
-                .Select(x => x.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries))
-                .Select(x => (x: int.Parse(x[0]), y: int.Parse(x[1])))
-                .ToList();
+            var points = GetLinesAsStrings()
+               .Select(x => x.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries))
+               .Select(x => (x: int.Parse(x[0]), y: int.Parse(x[1])))
+               .ToList();
 
-            var (xMin, xMax, yMin, yMax) = (points.Select(x => x.x).Min(),points.Select(x => x.x).Max(), points.Select(x => x.y).Min(), points.Select(x => x.y).Max());
+            var (xMin, xMax, yMin, yMax) = (points.Select(x => x.x).Min(), points.Select(x => x.x).Max(), points.Select(x => x.y).Min(), points.Select(x => x.y).Max());
             var count = 0;
             for (int i = xMin; i <= xMax; i++)
             {
                 for (int j = yMin; j <= yMax; j++)
                 {
-                    var distanceSum = points.Select(x => Distance((x.x, x.y), (i, j))).Sum();
-                    if (distanceSum < 10000)
+                    var sum = 0;
+                    for (int k = 0; k < points.Count; k++)
+                    {
+                        sum += Distance(points[k].x, points[k].y, i, j);
+                        if (sum >= 10000)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (sum < 10000)
                     {
                         count++;
                     }
@@ -71,6 +96,6 @@ namespace AdventOfCode.Days
             return count.ToString();
         }
 
-        private int Distance((int x, int y) pointA, (int x, int y) pointB) => Math.Abs(pointA.x - pointB.x) + Math.Abs(pointA.y - pointB.y);
+        private int Distance(int a, int b, int c, int d) => Math.Abs(a - c) + Math.Abs(b - d);
     }
 }
