06873df5afec9514bfa6552c329c274432a1c510
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-11-13 16:44:45 +0100

    Add project skeleton

--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Diagnostics;
+using AdventOfCode.Days;
 
 namespace AdventOfCode
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args is null || args.Length < 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a day number");
+                Console.ResetColor();
+                return;
+            }
+
+            Day day = GetDay(args[0]);
+
+            Console.WriteLine($"Day {args[0]}: {day.Title}{Environment.NewLine}");
+
+            var timer = new Stopwatch();
+
+            timer.Start();
+
+            string firstPart = day.ProcessFirst();
+            timer.Stop();
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write($"    {firstPart} ");
+            Console.ForegroundColor = GetColorForElapsedTime(timer.ElapsedMilliseconds);
+            Console.WriteLine($"({timer.ElapsedMilliseconds}ms)");
+            Console.ResetColor();
+
+            timer.Restart();
+
+            string secondPart = day.ProcessSecond();
+            timer.Stop();
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write($"    {secondPart} ");
+            Console.ForegroundColor = GetColorForElapsedTime(timer.ElapsedMilliseconds);
+            Console.WriteLine($"({timer.ElapsedMilliseconds}ms)");
+            Console.ResetColor();
+        }
+
+        private static Day GetDay(string dayNumber)
+        {
+            return dayNumber switch
+            {
+                _ => Day.Empty
+            };
+        }
+
+        private static ConsoleColor GetColorForElapsedTime(long elapsedMilliseconds)
+        {
+            return elapsedMilliseconds switch
+            {
+                < 750 => ConsoleColor.Green,
+                < 1250 => ConsoleColor.Yellow,
+                _ => ConsoleColor.Red,
+            };
         }
     }
 }
