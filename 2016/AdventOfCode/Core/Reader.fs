7a879accf5a29fd4b4e524c3e533afdb0373ef68
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-11-13 16:25:12 +0100

    Set up repository for 2019

--- 2016/AdventOfCode/Core/Reader.fs
+++
@@ -1,9 +0,0 @@
-module Reader
-
-open System.IO
-
-let readLines filePath = 
-    File.ReadLines(Path.Combine(@"D:\Developpement\Repos\AdventOfCode\2016\AdventOfCode\Inputs", filePath))
-
-let readInput filePath = 
-    File.ReadAllText(Path.Combine(@"D:\Developpement\Repos\AdventOfCode\2016\AdventOfCode\Inputs", filePath))
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # AdventOfCode
 
-Repository for my http://adventofcode.com/ solutions.
+Repository for my [Advent Of Code](http://adventofcode.com/) solutions for 2019.
+
+Past events can be found on the `past` branch.
