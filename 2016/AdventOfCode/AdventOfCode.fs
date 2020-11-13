8f9d970c783668e8466ef7e839417ba9198e74c4
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-11-13 16:25:12 +0100

    Set up repository for 2019

--- 2016/AdventOfCode/AdventOfCode.fs
+++
@@ -1,9 +0,0 @@
-module AdventOfCode
-
-open Day25
-
-[<EntryPoint>]
-let main argv =
-    computeFirst |> printfn "%A"
-    computeSecond |> printfn "%A"
-    0 // return an integer exit code
--- 2017/Program.fs
+++
@@ -1,9 +0,0 @@
-﻿module AdventOfCode
-
-open Day25
-
-[<EntryPoint>]
-let main argv =
-    computeFirst |> printfn "%A"
-    computeSecond |> printfn "%A"
-    0 // return an integer exit code
