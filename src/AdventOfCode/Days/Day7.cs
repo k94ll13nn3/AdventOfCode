09302c3d8f878529f85d3d3eb9355d4a90da452a
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-11-14 20:31:39 +0100

    Externalize Intcode logic

---
+++ src/AdventOfCode/Days/Day7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days
+{
+    public class Day7 : Day
+    {
+        public override string Title => "Amplification Circuit";
+
+        public override string ProcessFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ProcessSecond()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
