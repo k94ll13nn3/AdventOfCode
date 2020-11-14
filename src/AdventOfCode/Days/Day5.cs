75546ae08138c780e2f1d5fb3463b35b1f41d62b
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-11-14 11:29:31 +0100

    2019 - Day 5

---
+++ src/AdventOfCode/Days/Day5.cs
@@ -0,0 +1,104 @@
+﻿namespace AdventOfCode.Days
+{
+    public class Day5 : Day
+    {
+        public override string Title => "Sunny with a Chance of Asteroids";
+
+        public override string ProcessFirst()
+        {
+            return Compute(GetContentAsIntArray(','), 1, false).ToString();
+        }
+
+        public override string ProcessSecond()
+        {
+            return Compute(GetContentAsIntArray(','), 5, true).ToString();
+        }
+
+        private static int Compute(int[] program, int input, bool secondMode)
+        {
+            int pointer = 0;
+            int output = 0;
+            int par1 = 0;
+            int par2 = 0;
+            bool stop = false;
+            while (!stop)
+            {
+                string instruction = $"000{program[pointer]}";
+                switch (instruction[^2..])
+                {
+                    case "99":
+                        stop = true;
+                        break;
+
+                    case "01":
+                        par1 = instruction[^3] == '0' ? program[program[pointer + 1]] : program[pointer + 1];
+                        par2 = instruction[^4] == '0' ? program[program[pointer + 2]] : program[pointer + 2];
+                        program[program[pointer + 3]] = par1 + par2;
+                        pointer += 4;
+                        break;
+
+                    case "02":
+                        par1 = instruction[^3] == '0' ? program[program[pointer + 1]] : program[pointer + 1];
+                        par2 = instruction[^4] == '0' ? program[program[pointer + 2]] : program[pointer + 2];
+                        program[program[pointer + 3]] = par1 * par2;
+                        pointer += 4;
+                        break;
+
+                    case "03":
+                        program[program[pointer + 1]] = input;
+                        pointer += 2;
+                        break;
+
+                    case "04":
+                        output = instruction[^3] == '0' ? program[program[pointer + 1]] : program[pointer + 1];
+                        pointer += 2;
+                        break;
+
+                    case "05" when secondMode:
+                        par1 = instruction[^3] == '0' ? program[program[pointer + 1]] : program[pointer + 1];
+                        par2 = instruction[^4] == '0' ? program[program[pointer + 2]] : program[pointer + 2];
+                        if (par1 != 0)
+                        {
+                            pointer = par2;
+                        }
+                        else
+                        {
+                            pointer += 3;
+                        }
+
+                        break;
+
+                    case "06" when secondMode:
+                        par1 = instruction[^3] == '0' ? program[program[pointer + 1]] : program[pointer + 1];
+                        par2 = instruction[^4] == '0' ? program[program[pointer + 2]] : program[pointer + 2];
+                        if (par1 == 0)
+                        {
+                            pointer = par2;
+                        }
+                        else
+                        {
+                            pointer += 3;
+                        }
+
+                        break;
+
+                    case "07" when secondMode:
+                        par1 = instruction[^3] == '0' ? program[program[pointer + 1]] : program[pointer + 1];
+                        par2 = instruction[^4] == '0' ? program[program[pointer + 2]] : program[pointer + 2];
+                        program[program[pointer + 3]] = par1 < par2 ? 1 : 0;
+                        pointer += 4;
+                        break;
+
+                    case "08" when secondMode:
+                        par1 = instruction[^3] == '0' ? program[program[pointer + 1]] : program[pointer + 1];
+                        par2 = instruction[^4] == '0' ? program[program[pointer + 2]] : program[pointer + 2];
+                        program[program[pointer + 3]] = par1 == par2 ? 1 : 0;
+                        pointer += 4;
+                        break;
+                }
+            }
+
+            return output;
+        }
+    }
+}
--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -53,6 +53,7 @@ namespace AdventOfCode
                 "2" => new Day2(),
                 "3" => new Day3(),
                 "4" => new Day4(),
+                "5" => new Day5(),
                 _ => Day.Empty
             };
         }
