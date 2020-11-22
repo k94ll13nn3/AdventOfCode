185d1e87e9c9087ddb1af2562f7f5f97b8fb376c
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-11-22 15:53:50 +0100

    Prepare for 2020 edition

---
+++ src/AdventOfCode.Generator/ProgramGenerator.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace AdventOfCode.Generator
+{
+    [Generator]
+    public class ProgramGenerator : ISourceGenerator
+    {
+        private const string AttributeText = @"using System;
+
+namespace AdventOfCode.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class CoupledDayAttribute : Attribute
+    {
+        public CoupledDayAttribute(string firstAnswer, string secondAnswer)
+        {
+            FirstAnswer = firstAnswer;
+            SecondAnswer = secondAnswer;
+        }
+
+        public string FirstAnswer { get; set; }
+
+        public string SecondAnswer { get; set; }
+    }
+}
+";
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("CoupledDayAttribute", SourceText.From(AttributeText, Encoding.UTF8));
+
+            if (context.SyntaxReceiver is not SyntaxReceiver receiver)
+            {
+                return;
+            }
+
+            CSharpParseOptions? options = (context.Compilation as CSharpCompilation)?.SyntaxTrees[0].Options as CSharpParseOptions;
+            Compilation compilation = context.Compilation.AddSyntaxTrees(CSharpSyntaxTree.ParseText(SourceText.From(AttributeText, Encoding.UTF8), options));
+
+            INamedTypeSymbol? attributeSymbol = compilation.GetTypeByMetadataName("AdventOfCode.Attributes.CoupledDayAttribute");
+
+            StringBuilder sourceBuilder = new(@"using System;
+using System.Collections.Generic;
+using AdventOfCode.Days;
+
+namespace AdventOfCode
+{
+    internal static partial class Program
+    {
+        private static Day GetDay(string dayNumber)
+        {
+            return dayNumber switch
+            {
+");
+            foreach (ClassDeclarationSyntax candidateClass in receiver.CandidateClasses)
+            {
+                SemanticModel model = compilation.GetSemanticModel(candidateClass.SyntaxTree);
+                INamedTypeSymbol? symbol = model.GetDeclaredSymbol(candidateClass);
+
+                if (symbol is not null && symbol.BaseType?.ToString() == "AdventOfCode.Days.Day")
+                {
+                    sourceBuilder.AppendLine($@"                ""{candidateClass.Identifier.ToString().Substring(3)}"" => new {candidateClass.Identifier}(),");
+                }
+            }
+
+            sourceBuilder.Append(@"                _ => throw new InvalidOperationException(),
+            };
+        }");
+
+            sourceBuilder.Append(@"
+
+        private static List<(Day coupledDay, string firstAnswer, string secondAnswer)> GetCoupledDays()
+        {
+            return new()
+            {
+");
+
+            foreach (ClassDeclarationSyntax candidateClass in receiver.CandidateClasses)
+            {
+                SemanticModel model = compilation.GetSemanticModel(candidateClass.SyntaxTree);
+                INamedTypeSymbol? symbol = model.GetDeclaredSymbol(candidateClass);
+
+                AttributeData? attributeData = symbol?.GetAttributes().FirstOrDefault(ad => ad.AttributeClass?.Equals(attributeSymbol, SymbolEqualityComparer.Default) == true);
+                if (attributeData is not null)
+                {
+                    string firstAnswer = attributeData.ConstructorArguments[0].Value?.ToString() ?? "";
+                    string secondAnswer = attributeData.ConstructorArguments[1].Value?.ToString() ?? "";
+                    sourceBuilder.AppendLine($@"                (new {candidateClass.Identifier}(), ""{firstAnswer}"", ""{secondAnswer}""),");
+                }
+            }
+
+            sourceBuilder.Append(@"            };
+        }
+    }
+}");
+
+            context.AddSource("Program.Generated.cs", sourceBuilder.ToString());
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // Register a syntax receiver that will be created for each generation pass.
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+    }
+
+    /// <summary>
+    /// Created on demand before each generation pass
+    /// </summary>
+    internal class SyntaxReceiver : ISyntaxReceiver
+    {
+        public List<ClassDeclarationSyntax> CandidateClasses { get; } = new List<ClassDeclarationSyntax>();
+
+        /// <summary>
+        /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation.
+        /// </summary>
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is ClassDeclarationSyntax classDeclarationSyntax)
+            {
+                CandidateClasses.Add(classDeclarationSyntax);
+            }
+        }
+    }
+}
--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using AdventOfCode.Days;
 
 namespace AdventOfCode
 {
-    internal static class Program
+    internal static partial class Program
     {
         private static void Main(string[] args)
         {
@@ -39,14 +38,14 @@ namespace AdventOfCode
             ColorWrite($"    {secondPart} ", ConsoleColor.DarkGray);
             ColorWriteLine($"({timer.ElapsedMilliseconds}ms)", GetColorForElapsedTime(timer.ElapsedMilliseconds));
 
-            if (!Debugger.IsAttached && GetIntcodeDays().Any(x => x.intcodeDay.GetType() == day.GetType()))
+            if (!Debugger.IsAttached && GetCoupledDays().Any(x => x.coupledDay.GetType() == day.GetType()))
             {
                 Console.WriteLine();
-                Console.WriteLine("Checking Intcode...");
-                foreach ((Day intcodeDay, string firstAnswer, string secondAnswer) in GetIntcodeDays())
+                Console.WriteLine("Checking coupled days...");
+                foreach ((Day coupledDay, string firstAnswer, string secondAnswer) in GetCoupledDays())
                 {
-                    ColorWrite($"{intcodeDay.GetType().ToString().Split('.')[^1]} ({intcodeDay.Title}): ", ConsoleColor.DarkGray);
-                    if (intcodeDay.ProcessFirst() == firstAnswer)
+                    ColorWrite($"{coupledDay.GetType().ToString().Split('.')[^1]} ({coupledDay.Title}): ", ConsoleColor.DarkGray);
+                    if (coupledDay.ProcessFirst() == firstAnswer)
                     {
                         ColorWrite("OK", ConsoleColor.Green);
                     }
@@ -57,7 +56,7 @@ namespace AdventOfCode
 
                     ColorWrite("/", ConsoleColor.DarkGray);
 
-                    if (intcodeDay.ProcessSecond() == secondAnswer)
+                    if (coupledDay.ProcessSecond() == secondAnswer)
                     {
                         ColorWrite("OK", ConsoleColor.Green);
                     }
@@ -69,50 +68,6 @@ namespace AdventOfCode
                     Console.WriteLine();
                 }
             }
-        }
-
-        private static Day GetDay(string dayNumber)
-        {
-            return dayNumber switch
-            {
-                "1" => new Day1(),
-                "2" => new Day2(),
-                "3" => new Day3(),
-                "4" => new Day4(),
-                "5" => new Day5(),
-                "6" => new Day6(),
-                "7" => new Day7(),
-                "8" => new Day8(),
-                "9" => new Day9(),
-                "10" => new Day10(),
-                "11" => new Day11(),
-                "12" => new Day12(),
-                "13" => new Day13(),
-                "14" => new Day14(),
-                "15" => new Day15(),
-                "16" => new Day16(),
-                "17" => new Day17(),
-                "18" => new Day18(),
-                "19" => new Day19(),
-                "20" => new Day20(),
-                _ => throw new InvalidOperationException(),
-            };
-        }
-
-        private static List<(Day intcodeDay, string firstAnswer, string secondAnswer)> GetIntcodeDays()
-        {
-            return new()
-            {
-                (new Day2(), "4138687", "6635"),
-                (new Day5(), "2845163", "9436229"),
-                (new Day7(), "18812", "25534964"),
-                (new Day9(), "2457252183", "70634"),
-                (new Day11(), "2720", "SEE DRAWING (249)"),
-                (new Day13(), "333", "16539"),
-                (new Day15(), "224", "284"),
-                (new Day17(), "6448", "914900"),
-                (new Day19(), "116", "10311666"),
-            };
         }
 
         private static ConsoleColor GetColorForElapsedTime(long elapsedMilliseconds)
