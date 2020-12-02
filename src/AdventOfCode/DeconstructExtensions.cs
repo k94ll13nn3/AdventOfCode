64d4c9bf5066c30ce0b04b28fc37bd2f1df3ddca
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-12-02 10:07:03 +0100

    2020 - Day 2

---
+++ src/AdventOfCode/DeconstructExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    public static class DeconstructExtensions
+    {
+        public static void Deconstruct<T>(this IList<T> items, out T t0)
+        {
+            _ = items ?? throw new ArgumentNullException(nameof(items));
+            if (items.Count < 1)
+            {
+                throw new ArgumentException("Number of items must be more than 0.", nameof(items));
+            }
+
+            t0 = items[0];
+        }
+
+        public static void Deconstruct<T>(this IList<T> items, out T t0, out T t1)
+        {
+            _ = items ?? throw new ArgumentNullException(nameof(items));
+            if (items.Count < 2)
+            {
+                throw new ArgumentException("Number of items must be more than 1.", nameof(items));
+            }
+
+            t0 = items[0];
+            t1 = items[1];
+        }
+
+        public static void Deconstruct<T>(this IList<T> items, out T t0, out T t1, out T t2)
+        {
+            _ = items ?? throw new ArgumentNullException(nameof(items));
+            if (items.Count < 3)
+            {
+                throw new ArgumentException("Number of items must be more than 1.", nameof(items));
+            }
+
+            t0 = items[0];
+            t1 = items[1];
+            t2 = items[2];
+        }
+    }
+}
