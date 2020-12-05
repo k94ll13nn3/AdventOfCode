4f50a45f280aad2e8cf3591eab0ad540c545bad3
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-12-05 10:16:27 +0100

    2020 - Day 5

---
+++ src/AdventOfCode/Days/Day5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Days
+{
+    public class Day5 : Day
+    {
+        public override string Title => "Binary Boarding";
+
+        public override string ProcessFirst()
+        {
+            return $"{GetLines().Max(x => Convert.ToInt32(x.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1'), 2))}";
+        }
+
+        public override string ProcessSecond()
+        {
+            HashSet<int> seats = GetLines().Select(x => Convert.ToInt32(x.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1'), 2)).ToHashSet();
+
+            return $"{seats.Where(x => !seats.Contains(x - 1) || !seats.Contains(x + 1)).OrderBy(x => x).Skip(1).First() + 1}";
+        }
+    }
+}
