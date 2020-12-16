782e2adab7fede6d28a434fc3fd0861670269587
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-12-16 17:23:47 +0100

    2020 - Day 16

--- src/AdventOfCode/DeconstructExtensions.cs
+++ src/AdventOfCode/DeconstructExtensions.cs
@@ -40,5 +40,34 @@ namespace AdventOfCode
             t1 = items[1];
             t2 = items[2];
         }
+
+        public static void Deconstruct<T>(this IList<T> items, out T t0, out T t1, out T t2, out T t3)
+        {
+            _ = items ?? throw new ArgumentNullException(nameof(items));
+            if (items.Count < 4)
+            {
+                throw new ArgumentException("Number of items must be more than 1.", nameof(items));
+            }
+
+            t0 = items[0];
+            t1 = items[1];
+            t2 = items[2];
+            t3 = items[3];
+        }
+
+        public static void Deconstruct<T>(this IList<T> items, out T t0, out T t1, out T t2, out T t3, out T t4)
+        {
+            _ = items ?? throw new ArgumentNullException(nameof(items));
+            if (items.Count < 5)
+            {
+                throw new ArgumentException("Number of items must be more than 1.", nameof(items));
+            }
+
+            t0 = items[0];
+            t1 = items[1];
+            t2 = items[2];
+            t3 = items[3];
+            t4 = items[4];
+        }
     }
 }
