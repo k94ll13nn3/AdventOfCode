4bff8c54530be2c82b982cae1ccc7aada59a3cbc
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-12-01 07:54:05 +0100

    2021 - Day 1

---
+++ src/AdventOfCode/Day.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode;
+
+public abstract class Day
+{
+    protected Day(string dayNumber)
+    {
+        Content = File.ReadAllText($"Inputs/Input{dayNumber}.txt");
+        Lines = File.ReadAllLines($"Inputs/Input{dayNumber}.txt");
+    }
+
+    public abstract string Title { get; }
+
+    public string Content { get; }
+
+    public IEnumerable<string> Lines { get; }
+
+    public abstract string ProcessFirst();
+
+    public abstract string ProcessSecond();
+}
---
+++ src/AdventOfCode/Program.cs
@@ -0,0 +1,65 @@
+using System.Diagnostics;
+using AdventOfCode;
+using AdventOfCode.Days;
+
+string dayNumber = $"{DateTime.Now.Day}";
+if (args?.Length >= 1)
+{
+    dayNumber = args[0];
+}
+
+Day day = GetDay(dayNumber);
+
+Console.WriteLine($"Day {dayNumber}: {day.Title}");
+Console.WriteLine();
+
+var timer = new Stopwatch();
+
+timer.Start();
+
+string firstPart = day.ProcessFirst();
+timer.Stop();
+
+ColorWrite($"    {firstPart} ", ConsoleColor.DarkGray);
+ColorWriteLine($"({timer.ElapsedMilliseconds}ms)", GetColorForElapsedTime(timer.ElapsedMilliseconds));
+
+timer.Restart();
+
+string secondPart = day.ProcessSecond();
+timer.Stop();
+
+ColorWrite($"    {secondPart} ", ConsoleColor.DarkGray);
+ColorWriteLine($"({timer.ElapsedMilliseconds}ms)", GetColorForElapsedTime(timer.ElapsedMilliseconds));
+
+static ConsoleColor GetColorForElapsedTime(long elapsedMilliseconds)
+{
+    return elapsedMilliseconds switch
+    {
+        < 250 => ConsoleColor.Green,
+        < 500 => ConsoleColor.Yellow,
+        _ => ConsoleColor.Red,
+    };
+}
+
+static void ColorWrite(string text, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(text);
+    Console.ResetColor();
+}
+
+static void ColorWriteLine(string text, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(text);
+    Console.ResetColor();
+}
+
+static Day GetDay(string dayNumber)
+{
+    return dayNumber switch
+    {
+        "1" => new Day1(),
+        _ => throw new InvalidOperationException(),
+    };
+}
