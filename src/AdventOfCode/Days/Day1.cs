0be474e6e3ed83ca959f621e446148e984ee8277
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-12-01 07:54:05 +0100

    2021 - Day 1

---
+++ src/AdventOfCode/Days/Day1.cs
@@ -0,0 +1,42 @@
+namespace AdventOfCode.Days;
+
+public class Day1 : Day
+{
+    public Day1() : base("1")
+    {
+    }
+
+    public override string Title => "Sonar Sweep";
+
+    public override string ProcessFirst()
+    {
+        var measures = Lines.Select(int.Parse).ToList();
+
+        int count = 0;
+        for (int i = 1; i < measures.Count; i++)
+        {
+            if (measures[i] > measures[i - 1])
+            {
+                count++;
+            }
+        }
+
+        return $"{count}";
+    }
+
+    public override string ProcessSecond()
+    {
+        var measures = Lines.Select(int.Parse).ToList();
+
+        int count = 0;
+        for (int i = 3; i < measures.Count; i++)
+        {
+            if ((measures[i] + measures[i - 1] + measures[i - 2]) > (measures[i - 1] + measures[i - 2] + measures[i - 3]))
+            {
+                count++;
+            }
+        }
+
+        return $"{count}";
+    }
+}
