2c2bab290c97443e85c30fcd137c4a51dbcb4962
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-12-02 06:57:47 +0100

    Improve Day 1

--- src/AdventOfCode/Days/Day1.cs
+++ src/AdventOfCode/Days/Day1.cs
@@ -2,20 +2,21 @@ namespace AdventOfCode.Days;
 
 public class Day1 : Day
 {
+    private readonly List<int> _measures;
+
     public Day1() : base("1")
     {
+        _measures = Lines.Select(int.Parse).ToList();
     }
 
     public override string Title => "Sonar Sweep";
 
     public override string ProcessFirst()
     {
-        var measures = Lines.Select(int.Parse).ToList();
-
         int count = 0;
-        for (int i = 1; i < measures.Count; i++)
+        for (int i = 1; i < _measures.Count; i++)
         {
-            if (measures[i] > measures[i - 1])
+            if (_measures[i] > _measures[i - 1])
             {
                 count++;
             }
@@ -26,13 +27,11 @@ public class Day1 : Day
 
     public override string ProcessSecond()
     {
-        var measures = Lines.Select(int.Parse).ToList();
-
         int count = 0;
-        for (int i = 3; i < measures.Count; i++)
+        for (int i = 3; i < _measures.Count; i++)
         {
             // A + B + C > B + C + D <=> A > D
-            if (measures[i] > measures[i - 3])
+            if (_measures[i] > _measures[i - 3])
             {
                 count++;
             }
