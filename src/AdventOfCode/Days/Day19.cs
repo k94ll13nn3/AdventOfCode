c070eb49fccda2f49ac0e1a12828a39793ebb052
Kévin Gallienne <k94ll13nn3@outlook.fr>
2022-12-02 17:28:45 +0100

    Add 2021 last tries

---
+++ src/AdventOfCode/Days/Day19.cs
@@ -0,0 +1,19 @@
+namespace AdventOfCode.Days;
+public class Day19 : Day
+{
+    public Day19() : base("19")
+    {
+    }
+
+    public override string Title => "Beacon Scanner";
+
+    public override string ProcessFirst()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string ProcessSecond()
+    {
+        throw new NotImplementedException();
+    }
+}
