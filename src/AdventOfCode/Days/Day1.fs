66676be24d57eaede1e5488e599c008371ccb0d3
Kévin Gallienne <k94ll13nn3@outlook.fr>
2023-12-01 09:35:32 +0100

    Fix 2023 - Day 1 - Part 2

--- src/AdventOfCode/Days/Day1.fs
+++ src/AdventOfCode/Days/Day1.fs
@@ -15,40 +15,30 @@ let extractDigit (input: string) =
 
     (getFirstDigit 0 (+)) * 10 + (getFirstDigit (input.Length - 1) (-))
 
-let replaceNumber (input: string) =
-    input
-        .Replace("one", "1")
-        .Replace("two", "2")
-        .Replace("three", "3")
-        .Replace("four", "4")
-        .Replace("five", "5")
-        .Replace("six", "6")
-        .Replace("seven", "7")
-        .Replace("eight", "8")
-        .Replace("nine", "9")
 
-let rec replaceNumberInString (acc: string) (input: string) : string =
-    if input.Length = 0 then
-        replaceNumber acc
-    elif
-        acc.Contains("one")
-        || acc.Contains("two")
-        || acc.Contains("three")
-        || acc.Contains("four")
-        || acc.Contains("five")
-        || acc.Contains("six")
-        || acc.Contains("seven")
-        || acc.Contains("eight")
-        || acc.Contains("nine")
-    then
-        replaceNumberInString (String.Concat(replaceNumber acc, input.[0])) (input.[1..])
-    else
-        replaceNumberInString (String.Concat(acc, input.[0])) (input.[1..])
+let replaceNumbers (input: string) =
+    let replaceNumber (input: string) =
+        input
+            .Replace("one", "1e")
+            .Replace("two", "2o")
+            .Replace("three", "3e")
+            .Replace("four", "4")
+            .Replace("five", "5e")
+            .Replace("six", "6")
+            .Replace("seven", "7n")
+            .Replace("eight", "8t")
+            .Replace("nine", "9e")
+
+    let rec replaceNumberInString (acc: string) (input: string) =
+        if input.Length = 0 then
+            replaceNumber acc
+        else
+            replaceNumberInString (String.Concat(replaceNumber acc, input.[0])) (input.[1..])
+
+    replaceNumberInString "" input
 
 let computeFirst () =
     readLinesSeq "Input1.txt" |> Seq.map extractDigit |> Seq.sum
 
 let computeSecond () =
-    readLinesSeq "Input1.txt"
-    |> Seq.map (replaceNumberInString "" >> extractDigit)
-    |> Seq.sum
+    readLinesSeq "Input1.txt" |> Seq.map (replaceNumbers >> extractDigit) |> Seq.sum
