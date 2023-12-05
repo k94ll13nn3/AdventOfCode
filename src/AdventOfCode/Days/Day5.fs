c2d11e5c93134916340b3c3822de525f53aea894
Kévin Gallienne <k94ll13nn3@outlook.fr>
2023-12-05 20:54:23 +0100

    Optimize 2023 - Day 5

--- src/AdventOfCode/Days/Day5.fs
+++ src/AdventOfCode/Days/Day5.fs
@@ -1,6 +1,5 @@
 module Day5
 
-open System
 open Helper
 
 let number = 5
@@ -12,47 +11,51 @@ let lines = readLines "Input5.txt"
 let getRanges (input: _[]) =
     let rec getRangesRec index (rangesAcc: (uint64 * uint64 * uint64) list list) =
         match index with
-        | _ when index = input.Length -> rangesAcc |> List.rev |> List.tail // tail to remove extra [] from file line end
+        | _ when index >= input.Length -> rangesAcc
+        | _ when input.[index] = "" -> getRangesRec (index + 2) ([] :: rangesAcc)
         | _ ->
-            match input.[index] with
-            | "" -> getRangesRec (index + 1) ([] :: rangesAcc)
-            | _ ->
-                let parts = input.[index] |> splitC ' '
-
-                if parts.Length <> 3 then
-                    getRangesRec (index + 1) rangesAcc
-                else
-                    let h :: t = rangesAcc
-                    let newRange = (uint64 parts.[0], uint64 parts.[1], uint64 parts.[2])
-                    getRangesRec (index + 1) ((newRange :: h) :: t)
+            let parts = input.[index] |> splitC ' '
+            let h :: t = rangesAcc
+            let newRange = (uint64 parts.[0], uint64 parts.[1], uint64 parts.[2])
+            getRangesRec (index + 1) ((newRange :: h) :: t)
 
     getRangesRec 0 [ [] ]
 
-let applyRanges seed ranges =
-    let rec applyRangeRec i range =
+let computeFirst () =
+    let seed = lines.[0].[6..] |> splitA [| ' ' |] |> Array.map uint64
+    let ranges = getRanges lines.[1..] |> List.rev // ranges are stored last to first when reading input
+
+    let rec applyRange range i =
         match range with
         | [] -> i
         | (dest, src, len) :: _ when i >= src && i < (src + len) -> dest + (i - src)
-        | _ :: t -> applyRangeRec i t
+        | _ :: t -> applyRange t i
 
-    List.fold applyRangeRec seed ranges
-
-let applyRangesToSeedRange ranges seed =
-    Array.init (int (snd seed)) (fun v -> (uint64 v) + (fst seed))
-    |> Array.fold (fun acc seed -> ranges |> applyRanges seed |> min acc) UInt64.MaxValue
-
-let computeFirst () =
-    let ranges = getRanges lines.[1..]
-
-    lines.[0].[6..]
-    |> splitC ' '
-    |> Array.map (uint64 >> (fun i -> (i, 1UL)) >> applyRangesToSeedRange ranges)
+    ranges
+    |> List.fold (fun acc range -> acc |> Array.map (applyRange range)) seed
     |> Array.min
 
 let computeSecond () =
-    let seeds = lines.[0].[6..] |> splitC ' ' |> Array.map uint64
     let ranges = getRanges lines.[1..]
+    let seeds = lines.[0].[6..] |> splitC ' ' |> Array.map uint64
 
-    Array.init (seeds.Length / 2) (fun i -> (seeds.[i * 2], seeds.[(i * 2) + 1]))
-    |> Array.map (applyRangesToSeedRange ranges)
-    |> Array.min
+    let seedRanges =
+        List.init (seeds.Length / 2) (fun i -> (seeds.[i * 2], seeds.[(i * 2) + 1]))
+
+    let rec checkIfInSeedRange sr v =
+        match sr with
+        | [] -> false
+        | (str, len) :: _ when v >= str && v < (str + len) -> true
+        | _ :: t -> checkIfInSeedRange t v
+
+    let rec applyRangeReverseRec i range =
+        match range with
+        | [] -> i
+        | (dest, src, len) :: _ when i >= dest && i < (dest + len) -> src + (i - dest)
+        | _ :: t -> applyRangeReverseRec i t
+
+    let rec applyReverse i =
+        let res = List.fold applyRangeReverseRec i ranges |> checkIfInSeedRange seedRanges
+        if res then i else applyReverse (i + 1UL)
+
+    applyReverse 1UL
